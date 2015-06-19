f60e83f498c639706547730a52fadd651672fc42
Sean Dockery <sean@seandockery.org>
2015-06-19 09:21:07 -0600

    Replaced RedirectToRoute with File to transmit a file as the response.

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -14,7 +14,7 @@ namespace OdeToFood.Controllers
         public ActionResult Search(string name = "french")
         {
             var message = Server.HtmlEncode(name);
-            return RedirectToRoute("Default", new { controller = "Home", action = "About" });
+            return File(Server.MapPath("~/Content/Site.css"), "text/css");
         }
 
     }
