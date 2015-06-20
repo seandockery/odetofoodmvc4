577f404cd9fbd43b0215e4d7b7343149fb085b5e
Sean Dockery <sean@seandockery.org>
2015-06-20 00:43:47 -0600

    Revert to a single Search action in Cuisine controller, but require that the user have authenticated using the Authorize action filter.

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -11,18 +11,11 @@ namespace OdeToFood.Controllers
         //
         // GET: /Cuisine/
 
-        [HttpPost]
+        [Authorize]
         public ActionResult Search(string name = "french")
         {
             var message = Server.HtmlEncode(name);
             return Content(message);
         }
-
-        [HttpGet]
-        public ActionResult Search()
-        {
-            return Content("search");
-        }
-
     }
 }
