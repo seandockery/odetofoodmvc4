60b91b2a4b7d90eec944f9496a370814d04738ee
Sean Dockery <sean@seandockery.org>
2015-06-21 02:59:38 -0600

    Update the OdeToFoodDb DbContext to use the DefaultConnection connection defined in the Web.config.  Also, updated the file name of the mdf attached to the DefaultConnection.

--- OdeToFood/Models/OdeToFoodDb.cs
+++ OdeToFood/Models/OdeToFoodDb.cs
@@ -8,6 +8,9 @@ namespace OdeToFood.Models
 {
     public class OdeToFoodDb : DbContext
     {
+        public OdeToFoodDb() : base("name=DefaultConnection")
+        {
+        }
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<RestaurantReview> Reviews { get; set; }
     }
