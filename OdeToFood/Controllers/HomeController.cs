4d54b1de805dc63644feab0c1383a8fb183a9889
Sean Dockery <sean@seandockery.org>
2015-06-21 04:00:08 -0600

    Add support of a searchTerm for filtering the list of restaurants by name.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace OdeToFood.Controllers
     {
         OdeToFoodDb _db = new OdeToFoodDb();
 
-        public ActionResult Index()
+        public ActionResult Index(string searchTerm = null)
         {
             //var model =
             //    from r in _db.Restaurants
@@ -26,6 +26,7 @@ namespace OdeToFood.Controllers
             var model =
                 _db.Restaurants
                 .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
                 .Take(10)
                 .Select(r => new RestaurantListViewModel
                         {
