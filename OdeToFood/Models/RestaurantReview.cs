2b88987ade4c771a5fb01fe936caf49f9ba6e53c
Sean Dockery <sean@seandockery.org>
2015-06-25 01:58:21 -0600

    Demonstrate how to perform model-level validation rather than property-level validation by implementing the IValidatableObject interface.

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,32 +6,7 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
-    public class MaxWordsAttribute : ValidationAttribute
-    {
-        public MaxWordsAttribute(int maxWords)
-            : base("{0} has too many words.")
-        {
-            _maxWords = maxWords;
-        }
-
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var valueAsString = value.ToString();
-                if (valueAsString.Split(' ').Length > _maxWords)
-                {
-                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-                    return new ValidationResult(errorMessage);
-                }
-            }
-            return ValidationResult.Success;
-        }
-
-        private readonly int _maxWords;
-    }
-
-    public class RestaurantReview
+    public class RestaurantReview : IValidatableObject
     {
         public int Id { get; set; }
 
@@ -45,8 +20,15 @@ namespace OdeToFood.Models
 
         [Display(Name="User Name")]
         [DisplayFormat(NullDisplayText="Anonymous")]
-        [MaxWords(1)]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2 && ReviewerName.ToLower().StartsWith("scott"))
+            {
+                yield return new ValidationResult("Sorry, Scott, you can't do this", new string[] {"Rating"});
+            }
+        }
     }
 }
