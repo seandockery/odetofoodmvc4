251a851757debfa19e924def11e0355cd13fa521
Sean Dockery <sean@seandockery.org>
2015-06-25 01:48:46 -0600

    Limit the ReviewerName property to being a single word.

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,6 +6,31 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
+    public class MaxWordsAttribute : ValidationAttribute
+    {
+        public MaxWordsAttribute(int maxWords)
+            : base("{0} has too many words.")
+        {
+            _maxWords = maxWords;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Split(' ').Length > _maxWords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        private readonly int _maxWords;
+    }
+
     public class RestaurantReview
     {
         public int Id { get; set; }
@@ -20,6 +45,7 @@ namespace OdeToFood.Models
 
         [Display(Name="User Name")]
         [DisplayFormat(NullDisplayText="Anonymous")]
+        [MaxWords(1)]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
     }
