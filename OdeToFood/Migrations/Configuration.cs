9d98954f787b2864f1768ea4b1002687de7280cd
Sean Dockery <sean@seandockery.org>
2015-06-25 20:14:46 -0600

    Created a new ScriptBundle in BundleConfig that includes all scripts that will be included on every page.  Removed all occurrences of @Scripts.Render for JQuery validation in all views, and replaced @Scripts.Render for JQuery in the layout view with our new bundle.

--- OdeToFood/Migrations/Configuration.cs
+++ OdeToFood/Migrations/Configuration.cs
@@ -29,6 +29,12 @@ namespace OdeToFood.Migrations
                             new RestaurantReview { Rating = 9, Body = "Great food!", ReviewerName = "Sean" }
                         }
                 });
+
+            for (int i = 0; i < 1000; i++)
+            {
+                context.Restaurants.AddOrUpdate(r => r.Name,
+                    new Restaurant { Name = i.ToString(), City = "Nowhere", Country = "USA" });
+            }
         }
     }
 }
