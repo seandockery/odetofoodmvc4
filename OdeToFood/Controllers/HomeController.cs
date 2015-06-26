8392b4e03f6f4935ae49d890fa5493fa079454de
Sean Dockery <sean@seandockery.org>
2015-06-25 20:33:28 -0600

    Use an AJAX form in the Home controller's Index view to refresh only a section of the page rather than the entire page when searching for restaurants by name.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -37,6 +37,11 @@ namespace OdeToFood.Controllers
                             CountOfReviews = r.Reviews.Count()
                         });
 
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Restaurants", model);
+            }
+
             return View(model);
         }
 
