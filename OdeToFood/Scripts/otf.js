0c9bb4341d616ef7e5b0ee010ba436a8ff9d4a5f
Sean Dockery <sean@seandockery.org>
2015-06-26 23:07:46 -0600

    Added auto-complete suggestions as the user types a search term.

--- OdeToFood/Scripts/otf.js
+++ OdeToFood/Scripts/otf.js
@@ -11,12 +11,34 @@ $(function () {
 
         $.ajax(options).done(function (data) {
             var $target = $($form.attr("data-otf-target"));
-            $target.replaceWith(data);
+            var $newHtml = $(data);
+            $target.replaceWith($newHtml);
+            $newHtml.effect("highlight");
         });
 
         return false;
     };
 
+    var submitAutocompleteForm = function (event, ui) {
+        var $input = $(this);
+        $input.val(ui.item.label);
+
+        var $form = $input.parents("form:first");
+        $form.submit();
+    };
+
+    var createAutocomplete = function () {
+        var $input = $(this);
+
+        var options = {
+            source: $input.attr("data-otf-autocomplete"),
+            select: submitAutocompleteForm
+        };
+
+        $input.autocomplete(options);
+    };
+
     $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
 
+    $("input[data-otf-autocomplete]").each(createAutocomplete);
 });
