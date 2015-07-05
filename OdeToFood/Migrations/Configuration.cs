813321935078728eb5d4dc70a97e4c4f90bc6537
Sean Dockery <sean@seandockery.org>
2015-07-05 02:16:49 -0600

    See the setup of a user and the user's roles.

--- OdeToFood/Migrations/Configuration.cs
+++ OdeToFood/Migrations/Configuration.cs
@@ -6,6 +6,8 @@ namespace OdeToFood.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web.Security;
+    using WebMatrix.WebData;
 
     internal sealed class Configuration : DbMigrationsConfiguration<OdeToFood.Models.OdeToFoodDb>
     {
@@ -34,6 +36,29 @@ namespace OdeToFood.Migrations
             {
                 context.Restaurants.AddOrUpdate(r => r.Name,
                     new Restaurant { Name = i.ToString(), City = "Nowhere", Country = "USA" });
+            }
+
+            SeeMembership();
+        }
+
+        private void SeeMembership()
+        {
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+
+            var roles = (SimpleRoleProvider)Roles.Provider;
+            var membership = (SimpleMembershipProvider)Membership.Provider;
+
+            if (!roles.RoleExists("Admin"))
+            {
+                roles.CreateRole("Admin");
+            }
+            if (membership.GetUser("sallen", false) == null)
+            {
+                membership.CreateUserAndAccount("sallen", "imalittleteapot");
+            }
+            if (!roles.GetRolesForUser("sallen").Contains("Admin"))
+            {
+                roles.AddUsersToRoles(new[] { "sallen" }, new[] { "admin" });
             }
         }
     }
