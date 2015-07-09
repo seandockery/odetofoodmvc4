9304f88f524419a1e36e267c039c816e95da63b3
Sean Dockery <sean@seandockery.org>
2015-07-09 03:30:09 -0600

    Automatically configure the web site to use the user's culture information that is passed as an HTTP header.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace OdeToFood.Controllers
             return Json(model, JsonRequestBehavior.AllowGet);
         }
 
-        [OutputCache(CacheProfile="Long", VaryByHeader="X-Requested-With", Location = OutputCacheLocation.Server)]
+        [OutputCache(CacheProfile="Short", VaryByHeader="X-Requested-With", Location = OutputCacheLocation.Server)]
         public ActionResult Index(string searchTerm = null, int page = 1)
         {
             var model =
