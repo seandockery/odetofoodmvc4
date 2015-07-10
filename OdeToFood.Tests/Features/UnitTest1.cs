17513acaad53f90c70b15dfce8804c3ed285b726
Sean Dockery <sean@seandockery.org>
2015-07-10 01:28:04 -0600

    Added empty test to the unit testing project, along with comments as to what the business wants, as far as restaurant review values are concerned.

---
+++ OdeToFood.Tests/Features/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//
+// A restaurant's overall rating can be calculating using various methods.
+// For this application we'll want to try different methods over time,
+// but for starters we'll allow an administrator to toggle between two
+// different techniques.
+//
+// 1. Simple mean of the "rating" value for the most recent n reviews
+//    (the admin can configure the value n).
+//
+// 2. Weighted mean of the last n reviews.  The most recent n/2 reviews
+//    will be weighted twice as much as the oldest n/2 reviews.
+//
+// Overall rating should be a whole number.
+
+namespace OdeToFood.Tests.Features
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
