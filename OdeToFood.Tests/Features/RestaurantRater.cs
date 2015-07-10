2129fa22985c011424f3275f063cf4259edbfcf2
Sean Dockery <sean@seandockery.org>
2015-07-10 01:42:11 -0600

    Generated all classes, constructors, and methods to avoid build errors.

--- OdeToFood.Tests/Features/RestaurantRater.cs
+++ OdeToFood.Tests/Features/RestaurantRater.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using OdeToFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace OdeToFood.Tests.Features
 {
-    class RestaurantRater
+    public class RestaurantRater
     {
+        private Restaurant data;
+
+        public RestaurantRater(Restaurant data)
+        {
+            // TODO: Complete member initialization
+            this.data = data;
+        }
+
+        public RatingResult ComputeRating(int p)
+        {
+            return new RatingResult();
+        }
     }
 }
